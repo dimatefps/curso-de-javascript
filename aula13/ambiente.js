ea687726fcd979b9fd41d5cb74c0d7a82cf792a4
Mate <matheusryan406@gmail.com>
2026-03-29 14:06:04 -0300

    aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+count = 1
+do {
+    console.log('imprimindo com do')
+    count++
+} while (count <= 6 )
+
+
+
+
+// var count = 1
+// while (count <= 6){
+//     console.log('OLá, Mundo!')
+//     count++
+// }
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for (var i = 1; i <=5; i++){
+    console.log(i)
+}
+console.log('FIM!')
