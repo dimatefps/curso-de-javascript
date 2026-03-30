59f07cca0a543e192c27e82a69e7b1154d03a9a9
Mate <matheusryan406@gmail.com>
2026-03-30 15:44:44 -0300

    considera passo negativo e outras coisas

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,10 +2,13 @@ function calcular(){
     var inicio = document.getElementById('inicio').value
     var fim = document.getElementById('fim').value
     var passo = document.getElementById('passo').value
-
+    
     var res = document.getElementById('res')
-
-
+    
+    
+    if (inicio.length == 0 || fim.length == 0 || passo.length == 0){
+        res.innerHTML = 'Impossível contar!'   
+    } else {
     
     if (passo == 0){
         window.alert('Passo inválido! Considerando passo 1')
@@ -13,14 +16,19 @@ function calcular(){
     }
 
     var texto = "Contando: \n"
-    for (var i = Number(inicio); i <= Number(fim); i += Number(passo)){
-        texto += `${i} \u{1f449}`
+
+    if (fim < inicio){
+        for (var i = Number(inicio); i >= Number(fim); i -= Math.abs(Number(passo))){
+            texto += `${i} \u{1f449}`
+        }
+    } else {
+        for (var i = Number(inicio); i <= Number(fim); i += Math.abs(Number(passo))){
+            texto += `${i} \u{1f449}`
+        }
     }
     texto += '\u{1f3c1}'
 
     res.innerHTML = ''
     res.innerHTML = texto
-    if (inicio.length == 0 || fim.length == 0 || passo.length == 0){
-        res.innerHTML = 'Impossível contar!'   
-    }
+}
 }
