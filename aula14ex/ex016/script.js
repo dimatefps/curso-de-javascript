b7dc22478fe8c9c963e17085045d198fb292cde2
Mate <matheusryan406@gmail.com>
2026-03-30 14:55:35 -0300

    ex016

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,26 @@
+function calcular(){
+    var inicio = document.getElementById('inicio').value
+    var fim = document.getElementById('fim').value
+    var passo = document.getElementById('passo').value
+
+    var res = document.getElementById('res')
+
+
+    
+    if (passo == 0){
+        window.alert('Passo inválido! Considerando passo 1')
+        passo = 1
+    }
+
+    var texto = "Contando: \n"
+    for (var i = Number(inicio); i <= Number(fim); i += Number(passo)){
+        texto += `${i} 👉`
+    }
+    texto += '🏁'
+
+    res.innerHTML = ''
+    res.innerHTML = texto
+    if (inicio.length == 0 || fim.length == 0 || passo.length == 0){
+        res.innerHTML = 'Impossível contar!'   
+    }
+}
